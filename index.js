3dd5de0a8098eadfbe0682eda95651684ecdb488
abdul-rahman-1999 <rahmanabdul10@outlook.com>
2022-11-15 20:15:35 +0000

    data

--- index.js
+++ index.js
@@ -16,34 +16,68 @@ app.get("/", function (request, response) {
   response.send("🙋‍♂️, 🌏 🎊✨🤩");
 });
 
-// const Hall = [{
-//   "id": "1",
-//   "roomID": "200",
-//   "roomName": "Oneplus",
-//   "customerName":"Abdul",
-//   "date":new Date("2022-10-15"),
-//   "startTime":"10:00",
-//   "endTime":"12:00",
-//   },
+
+// For creating Room 
+
+// const room = [
 //   {
-//     "id": "2",
-//     "roomID": "201",
-//     "roomName": "Redmi",
-//     "customerName":"Ashok",
-//     "date":new Date("2022-10-13"),
-//     "startTime":"08:00",
-//     "endTime":"12:00",
+//       "id":"1",
+//       "fk":"101",
+//       "roomName":"High",
+//       "noOfSeats":"5",
+//       "amentities":"Air Conditioner, Balcony, Smart TV, Telephone, Free Wifi, Iron Box, Water Heater",
+//       "priceForeOneHour":"Rs.1000",
+//       "status":"Booked"
+//   },
+//       {
+//           "id":"2",
+//           "fk":"102",
+//           "roomName":"Medium",
+//       "noOfSeats":"5",
+//       "amentities":"Balcony, Smart TV, Telephone, Water Heater",
+//       "priceForeOneHour":"Rs.600",
+//       "status":"Booked"
+//   },
+//           {
+//               "id":"3",
+//               "fk":"103",
+//               "roomName":"Low",
+//       "noOfSeats":"5",
+//       "amentities":"Balcony, Telephone, Water Heater",
+//       "priceForeOneHour":"Rs.400",
+//       "status":"Booked"
+//   }
+// ]
+
+// For Booking
+
+// const booking = [
+//   {
+//     "id": "1",
+//     "roomID": "101",
+//     "customerName":"Krishnan",
+//     "date":"2022-11-30T18:25:43.511Z",
+//     "startTime":"11:00",
+//     "endTime":"22:00"
 //     },
 //     {
+//       "id": "2",
+//       "roomID": "102",
+//       "customerName":"Krishnan",
+//       "date":"2022-11-20T18:25:43.511Z",
+//       "startTime":"10:00",
+//       "endTime":"20:00"
+//       },
+//       {
 //         "id": "3",
-//         "roomID": "202",
-//         "roomName": "Oppo",
-//         "customerName":"Prasanna",
-//         "date":new Date("2022-10-14"),
-//         "startTime":"12:00",
-//         "endTime":"20:00",
+//         "roomID": "103",
+//         "customerName":"Krishnan",
+//         "date":"2022-11-10T18:25:43.511Z",
+//         "startTime":"09:00",
+//         "endTime":"16:00"
 //         }
-//   ]
+// ]
+
 
 app.use(express.json())
 app.use('/booking', bookingRouter)
